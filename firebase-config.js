c2c401fc5e837ad266c3e8c319edd48aa96b7aac
Tinel25 <108215117+Tinel25@users.noreply.github.com>
2025-08-20 20:10:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  databaseURL: "https://TON_PROJET.firebaseio.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyAibnn8YMMiEM0mJfOQRJ_8taGZ4IqbvXR0",
+  authDomain: "audiominecraftentertainment.firebaseapp.com",
+  projectId: "audiominecraftentertainment",
+  storageBucket: "audiominecraftentertainment.appspot.com",
+  messagingSenderId: "557660028873",
+  appId: "1:557660028873:web:29f9295cf72fb0f506fb218",
+  measurementId: "G-HFQ98YBQS8"
 };
 
 firebase.initializeApp(firebaseConfig);
+
